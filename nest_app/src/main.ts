0e39456d8482cdd3c996a23b0698d037658aa995
Thomas Sales <thomas.sales@testia.com>
2024-09-03 16:38:22 +0200

    reorganize folder structure

--- nest_app/src/main.ts
+++ nest_app/src/main.ts
@@ -2,7 +2,8 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
+  const port = process.env.NEST_RUNNING_PORT
   const app = await NestFactory.create(AppModule);
-  await app.listen(3003);
+  await app.listen(port);
 }
 bootstrap();
