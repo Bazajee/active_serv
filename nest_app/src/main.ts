03230fe77a5e0bd8d2dbb122ec832b1aa589a24b
Thomas Sales <thomas.sales@testia.com>
2024-09-03 18:05:39 +0200

    fix port var

--- nest_app/src/main.ts
+++ nest_app/src/main.ts
@@ -4,6 +4,6 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const port = process.env.NEST_RUNNING_PORT
   const app = await NestFactory.create(AppModule);
-  await app.listen(port);
+  await app.listen(3003);
 }
 bootstrap();
